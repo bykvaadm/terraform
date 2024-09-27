6acbd1a25a26e58744d89f008ba9b0f6ed79a2da
Kondratev Alexander <a.kondratev@bi.zone.ru>
2024-09-27 21:18:39 +0300

    release for students

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ terraform {
     yandex = {
       source = "yandex-cloud/yandex"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
   }
   required_version = ">= 0.13"
 }
@@ -13,14 +17,6 @@ provider "yandex" {
 
 resource "yandex_compute_disk" "boot-disk-1" {
   name     = "boot-disk-1"
-  type     = "network-hdd"
-  zone     = "ru-central1-a"
-  size     = "20"
-  image_id = "fd83j4siasgfq4pi1qif"
-}
-
-resource "yandex_compute_disk" "boot-disk-2" {
-  name     = "boot-disk-2"
   type     = "network-hdd"
   zone     = "ru-central1-a"
   size     = "20"
@@ -48,71 +44,6 @@ resource "yandex_compute_instance" "vm-1" {
     user-data = "${file("/Users/a.kondratev/Documents/Documents - NB0571/cyberED/1/meta.txt")}"
   }
 
-  connection {
-    type     = "ssh"
-    user     = "debian"
-    #password = var.root_password
-    host     = self.network_interface.0.nat_ip_address
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update",
-      "sudo apt -y upgrade",
-      "sudo apt-get install ca-certificates curl git",
-      "sudo install -m 0755 -d /etc/apt/keyrings",
-      "sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc",
-      "sudo chmod a+r /etc/apt/keyrings/docker.asc",
-      "echo deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian   bookworm stable > /etc/apt/sources.list.d/docker.list",
-      "sudo apt-get update",
-      "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
-    ]
-  }
-
-}
-
-resource "yandex_compute_instance" "vm-2" {
-  name = "terraform2"
-
-  resources {
-    cores  = 4
-    memory = 4
-  }
-
-  boot_disk {
-    disk_id = yandex_compute_disk.boot-disk-2.id
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}"
-  }
-
-  connection {
-    type     = "ssh"
-    user     = "debian"
-    #password = var.root_password
-    host     = self.network_interface.0.nat_ip_address
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update",
-      "sudo apt -y upgrade",
-      "sudo apt-get install ca-certificates curl git",
-      "sudo install -m 0755 -d /etc/apt/keyrings",
-      "sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc",
-      "sudo chmod a+r /etc/apt/keyrings/docker.asc",
-      "echo deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian   bookworm stable > /etc/apt/sources.list.d/docker.list",
-      "sudo apt-get update",
-      "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
-    ]
-  }
-
 }
 
 resource "yandex_vpc_network" "network-1" {
@@ -130,32 +61,36 @@ output "internal_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
 
-output "internal_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
-}
-
 output "external_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
 
-output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+resource "local_file" "private_key" {
+  content  = "[osquery]\nosquery-1 ansible_host=${yandex_compute_instance.vm-1.network_interface.0.nat_ip_address} ansible_user=debian"
+  filename = "ansible/hosts"
 }
 
-#provisioner "remote-exec" {
-#inline = [
-#sudo apt-get update
-#sudo apt-get install ca-certificates curl
-#sudo install -m 0755 -d /etc/apt/keyrings
-#sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o
-#/etc/apt/keyrings/docker.asc
-#sudo chmod a+r /etc/apt/keyrings/docker.asc
-## Add the repository to Apt sources:
-#echo \
-#"deb [arch=$(dpkg --print-architecture) signed-
-#by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-#$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-#sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#sudo apt-get update
-#]
-#}
+resource "null_resource" "baz" {
+  connection {
+    type = "ssh"
+    user = "debian"
+    #password = var.root_password
+    host = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt -y install ca-certificates curl git",
+      "sudo install -m 0755 -d /etc/apt/keyrings",
+      "sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc",
+      "sudo chmod a+r /etc/apt/keyrings/docker.asc",
+      "echo deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian   bookworm stable | sudo tee /etc/apt/sources.list.d/docker.list",
+      "sudo apt update",
+      "sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
+    ]
+  }
+  triggers = {
+    always_run = timestamp()
+  }
+}
